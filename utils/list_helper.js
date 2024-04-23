9f41725beb2716d01dfd0998e5419757d5627ae4
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-04-23 17:25:05 +0300

    4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    // taulukko blogeja
+    // jokaiselle taulukon blogille
+    // otetaan sen tykkäykset eli likes
+    // lasketaan yhteen ne
+    const likes = blogs.reduce((acc, curr) => {
+        return acc + curr.likes
+    }, 0)
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
