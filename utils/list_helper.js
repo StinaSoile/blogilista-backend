1bda7b54926135f9cd204b60c23a056d6e5b9f2e
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-04-23 21:49:10 +0300

    4.7 asti done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -13,7 +13,72 @@ const totalLikes = (blogs) => {
     return likes
 }
 
+/**
+ * 
+ * @param {{likes: number, title: string, author: string}[]} blogs 
+ * @returns 
+ */
+const favouriteBlog = (blogs) => {
+    // taulukko blogeja
+    // sortataan se likesin mukaan:
+    // palautetaan eka blogi. 
+    if (blogs.length === 0) {
+        return undefined
+    }
+    const sortedList = [...blogs].sort((a, b) => (b.likes - a.likes))
+
+    const favouriteBlog = sortedList[0]
+    return {
+        title: favouriteBlog.title,
+        author: favouriteBlog.author,
+        likes: favouriteBlog.likes
+    }
+}
+
+/**
+ * 
+ * @param {{}[]}} blogs 
+ * @returns
+ */
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return undefined
+    const writers = {}
+    blogs.map(blog => {
+        if (writers[blog.author]) writers[blog.author]++
+        else writers[blog.author] = 1
+    })
+    const writerArray = Object.entries(writers).sort(([, a], [, b]) => b - a)
+
+    return {
+        author: writerArray[0][0],
+        blogs: writerArray[0][1]
+    }
+}
+
+/**
+ * 
+ * @param {{}[]} blogs 
+ * @returns {author: string ,likes: number  }
+ */
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) return undefined
+    const writers = {}
+    blogs.map(blog => {
+        if (writers[blog.author]) writers[blog.author] = writers[blog.author] + blog.likes
+        else writers[blog.author] = blog.likes
+    })
+    const writerArray = Object.entries(writers).sort(([, a], [, b]) => b - a)
+
+    return {
+        author: writerArray[0][0],
+        likes: writerArray[0][1]
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
